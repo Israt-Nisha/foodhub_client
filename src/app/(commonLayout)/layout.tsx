fb6ce936601e3e1adc440778d6fe6c87624e60d6
Israt-Nisha <isratnisha1999@gmail.com>
2026-01-30 21:32:57 +0600

    setup project and create navbar and login and register form

---
+++ src/app/(commonLayout)/layout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "@/components/layout/Navbar";
+import { ReactNode } from "react";
+
+
+export default function CommonLayout({children,}: {
+    children: ReactNode ;
+}) {
+    return(
+        <div>
+            <Navbar />
+            {children}
+        </div>
+    )
+ }
